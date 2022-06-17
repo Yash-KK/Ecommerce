723d75b6ae3866f76eff2f940a9d33d09f8acbfe
Yash Kharche <reachyashk@gmail.com>
2022-06-17 18:19:14 +0530

    More Cart functionality

--- greatKart/Store/views.py
+++ greatKart/Store/views.py
@@ -1,3 +1,4 @@
+from itertools import product
 from msilib.schema import Error
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -5,6 +6,11 @@ from django.shortcuts import render
 from Store.models import (
     Product
 )
+from Cart.models import (
+    Cart,
+    CartItem
+)
+from Cart.views import _cart_id
 # Create your views here.
 def store(request,category_slug=None):
     if category_slug:
@@ -20,12 +26,15 @@ def store(request,category_slug=None):
     }
     return render(request,'Store/store.html',context)
 
-def product_detail(request,category_slug,product_slug):
+ 
+def product_detail(request,category_slug,product_slug):    
     try:
         single_product = Product.objects.get(category__slug=category_slug,p_slug=product_slug)
+        cart_item = CartItem.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()
     except Exception as Error:
         raise Error  
     context = {
-        'single_product':single_product
-    }
+        'single_product':single_product,
+        'cart_item':cart_item
+    } 
     return render(request,'Store/product_detail.html',context)
