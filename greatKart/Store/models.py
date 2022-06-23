00c92d2cddbfd55d7942c1c8f293e21414e0baa5
Yash Kharche <reachyashk@gmail.com>
2022-06-23 08:35:48 +0530

    Search Functionality

--- greatKart/Store/models.py
+++ greatKart/Store/models.py
@@ -7,11 +7,11 @@ class Product(models.Model):
     product_name = models.CharField(max_length=100,unique=True)
     p_slug = models.SlugField(max_length=100,unique=True)
     description = models.TextField(max_length=500)
-    pro_images = models.ImageField(upload_to='photo/Products/')
+    pro_images = models.ImageField(upload_to='photo/Products/') 
     price = models.IntegerField()
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
-    category = models.ForeignKey(Category,on_delete=models.CASCADE) 
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)  
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     
--- greatKart/Store/urls.py
+++ greatKart/Store/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("",views.store,name='s-store'),
+    path("search/",views.search,name='search'),
     path("<slug:category_slug>/",views.store,name='products-by-category'),
     path("<slug:category_slug>/<slug:product_slug>/",views.product_detail,name='product-detail')
 
